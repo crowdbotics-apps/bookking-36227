bbb756111a8b9aa1c866265a29c3daade962824e
Crowdbotics <team@crowdbotics.com>
2022-07-17 01:53:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "bookking_36227",
+  url: "https://bookking_36227.botics.co",
+  api: "https://bookking_36227.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
